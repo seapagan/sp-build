66754075e7d3e860f2b13970738eee952296c33e
Grant Ramsay <seapagan@gmail.com>
2022-07-28 18:15:23 +0100

    update the HTML placeholder

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@
 import "./styles/site.css";
 
 // prove that our JS file is loaded, remove for production!
-console.log("JavaScript Loaded!");
+const msg = "If you can read this, and the text below is in a red box then"+
+" the JavaScript and CSS are imported properly.";
+document.getElementById("js-test").innerText = msg;
+document.querySelector(".no-js").style.display = "none";
