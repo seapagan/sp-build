c071c58040eb5ddb2bb622d2bd2bd63191ebc41b
Grant Ramsay <seapagan@gmail.com>
2022-08-18 12:05:46 +0100

    add .env.example and test in JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 // import the main CSS. This needs to be here
 import "./styles/site.css";
 
-// prove that our JS file is loaded, remove for your own projects!
-const msg = "If you can read this, and the text below is in a blue box then"+
-" the JavaScript and CSS are imported properly.";
+/* -------------------------------------------------------------------------- */
+/*                remove everything below for your own projects               */
+/* -------------------------------------------------------------------------- */
+
+// prove that our JS file is loaded
+const msg =
+  "If you can read this, and the text below is in a blue box then" +
+  " the JavaScript and CSS are imported properly.";
 document.getElementById("js-test").innerText = msg;
 document.querySelector(".no-js").style.display = "none";
+
+// Testing that the .env file is read
+console.log(process.env.TEST_ENV_STRING);
+console.log(process.env.TEST_ENV_VAR);
