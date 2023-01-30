ff3958d23f1507c1322742dab247bd0c74c6d657
Grant Ramsay <seapagan@gmail.com>
2023-01-30 18:21:02 +0000

    add another example test

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,18 @@
+beforeEach(() => {
+  // set up the dom so the test has something to access
+  document.body.innerHTML = `
+    <p data-testid="js-test" id='js-test'></p>
+  `;
+});
+
 test("This test file is run properly", () => {
   expect(true).toBeTruthy();
+});
+
+test("it should output to the console 3 times", () => {
+  const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+
+  require("./index.js");
+
+  expect(consoleSpy).toHaveBeenCalledTimes(3);
 });
