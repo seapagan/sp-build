88e1990cc3be49fafb3fcc95e240e933b819e09a
Grant Ramsay <seapagan@gmail.com>
2023-01-30 11:32:09 +0000

    move test folder and __mocks__ into src folder

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   moduleNameMapper: {
     // eslint-disable-next-line
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
+      "<rootDir>/src/tests/__mocks__/fileMock.js",
     // "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
     "\\.(sa|sc|c)ss$": "identity-obj-proxy",
   },
